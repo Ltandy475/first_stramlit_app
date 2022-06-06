f395958fc49bbac0ba3fa265d2df7fc5bc2f009b
Ltandy475 <Ltandy475@users.noreply.github.com>
2022-06-06 17:17:51 -0400

    Update streamlit_app.py
    added the multi picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,11 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 import pandas
+                                       
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.setindex('Fruit;)
+
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.dataframe(my_fruit_list)
