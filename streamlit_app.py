8343f3a00e9a11b533b0fb44fbd433a8e8e375aa
Ltandy475 <Ltandy475@users.noreply.github.com>
2022-06-06 16:39:36 -0400

    Update streamlit_app.py
    Fixed Typos

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.text('🥣 mega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 ale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 ard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 import pandas
